fb08d6e888dc0cd74d3db1aac30a222ed7305eaf
Macmenamin, John <John.Macmenamin@aig.com>
2017-12-18 09:44:14 -0600

    updating tut files

--- src/components/App.js
+++ src/components/App.js
@@ -1,45 +1,22 @@
-import React, { Component } from 'react';
-import Display from './Display';
+import React, { Component } from "react"
+import Display from "./Display"
 
 class App extends Component {
   state = {
-    postList: [
-      {
-        id: 0,
-        title: "Hello, How Are You?",
-        body: "There was a dog and it was brown.",
-        postsCount: 17,
-      },
-      {
-        id: 1,
-        title: "A Great Long Title",
-        body: "With much redundancy the man spoke.",
-        postsCount: 9,
-      },
-      {
-        id: 2,
-        title: "Sample Business Cards",
-        body: "The best time to buy things is tomorrow.",
-        postsCount: 21,
-      },
-    ],
+    title: "Hello, How Are You?",
+    body: "There was a dog and it was brown.",
+    postsCount: 17,
   }
 
-  vote = (id,voteType) => {
-    const updatedPostList = this.state.postList.map((post => {
-      if(post.id !== id) return post;
-
-      const updatedPostCount = voteType === 'up' ? post.postsCount +1 : post.postsCount -1
-
-      return({
-        ...post,
-        postsCount: updatedPostCount,
-      });
-    }));
+  upVote = () => {
     this.setState({
-      postList: updatedPostList,
-    });
-    //console.log(updatedPostList);
+      postsCount: this.state.postsCount +1,
+    })
+  }
+  downVote = () => {
+    this.setState({
+      postsCount: this.state.postsCount -1,
+    })
   }
 
   render() {
@@ -48,12 +25,15 @@ class App extends Component {
         <h2>Container Component</h2>
         <br />
         <Display
-          posts={this.state.postList}
-          vote={this.vote}
+          title={this.state.title}
+          body={this.state.body}
+          postsCount={this.state.postsCount}
+          upVote={this.upVote}
+          downVote={this.downVote}
         />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/components/Display.js
+++ src/components/Display.js
@@ -1,14 +1,13 @@
-import React from 'react'
+import React from "react"
 
-const Display = ({ posts,vote }) => (
+const Display = ({
+  title, body, postsCount, upVote, downVote,
+}) => (
   <div className="">
-    {posts.map(({id, title, body, postsCount }) => (
-      <div className="" key={id}>
-        <h4>{title}</h4>
-        <em><strong>Likes:</strong> {postsCount}</em> <button onClick={() => vote(id,'up')}>+1</button> <button onClick={() => vote(id, 'down')}>-1</button>
-        <br /><br />
-      </div>
-    ))}
+    <h2>{title}</h2>
+    <p>{body}</p>
+    <em>Post Count: {postsCount}</em>
+    <button onClick={() => upVote()}>+1</button><button onClick={() => downVote()}>-1</button>
   </div>
 )
 
